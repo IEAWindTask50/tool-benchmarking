e1cd7f234239ab604a629137a757e1bd5f216c24
mikf <mikf@dtu.dk>
2024-02-02 18:28:17 +0100

    added: European examples for France, Germany and Denmark HyDesign2HOPP input converter draft Example notebook for France case draft

---
+++ Europe/hyd2hopp_input_converter.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import pandas as pd
+
+#%% WIND
+
+hopp_ref_wind_path = r"C:\Sandbox\Repo\NREL\HOPP\resource_files\wind\35.2018863_-101.945027_windtoolkit_2012_60min_80m_100m.srw"
+hopp_ref_df = pd.read_csv(hopp_ref_wind_path, skiprows=[0,1,3,4])
+
+hopp_path = r"C:\Sandbox\Repo\NREL\HOPP\resource_files\wind\france.srw"
+
+hyd_wind_path = r"C:\Sandbox\Repo\TOPFARM\hydesign\hydesign\examples\Europe\GWA2\input_ts_France_good_wind.csv"
+hyd_df = pd.read_csv(hyd_wind_path)
+hyd_df = hyd_df.truncate(after=8759)
+
+hyd_2_hopp_wind_map = {'temp_air_1': 'Temperature',
+                       'WS_50': 'Speed',
+                      'WD_50': 'Direction',
+                      'WS_100': 'Speed.1', 
+                      'WD_100': 'Direction.1'} ## The pressures are not in HyDeisgn
+
+hopp_df = hopp_ref_df.copy()
+for k, v in hyd_2_hopp_wind_map.items():
+    hopp_df[v] = hyd_df[k]
+    
+hopp_df['Temperature'] -=273.15  # Hydesign have Kelvin
+hopp_df.to_csv(hopp_path, sep=',', index=False) ## Note there is a few lines in the top of the file that needs to be added
+
+#%% SOLAR
+
+hopp_ref_solar_path = r"C:\Sandbox\Repo\NREL\HOPP\resource_files\solar\35.2018863_-101.945027_psmv3_60_2012.csv"
+hopp_ref_solar_df = pd.read_csv(hopp_ref_solar_path, skiprows=[0,1])
+
+hopp_solar_path = r"C:\Sandbox\Repo\NREL\HOPP\resource_files\solar\france.csv"
+
+hyd_2_hopp_solar_map = {'ghi': 'GHI',
+                        'dhi': 'DHI',
+                        'dni': 'DNI',
+                        'WS_100': 'Wind Speed', 
+                        'temp_air_1': 'Temperature', 
+                        # 'Solar Zenith Angle',   Why is this not in HyDesign
+                        # 'Pressure',
+                       # 'Dew Point'
+                       }
+
+hopp_solar_df = hopp_ref_solar_df.copy()
+for k, v in hyd_2_hopp_solar_map.items():
+    hopp_solar_df[v] = hyd_df[k]
+    
+hopp_solar_df['Temperature'] -= 273.15  # Hydesign have Kelvin
+hopp_solar_df.to_csv(hopp_solar_path, sep=',', index=False) ## Note there is a few lines in the top of the file that needs to be added
